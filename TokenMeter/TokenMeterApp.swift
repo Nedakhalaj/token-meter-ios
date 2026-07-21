0b170d0835129a1289371c2277433e5a6a6a7c84
Nedakhalaj <neda.khalaj7168@gmail.com>
2026-07-21 12:44:41 +0200

    Initial Commit

---
+++ TokenMeter/TokenMeterApp.swift
@@ -0,0 +1,17 @@
+//
+//  TokenMeterApp.swift
+//  TokenMeter
+//
+//  Created by neda khalajnejad on 2026-07-21.
+//
+
+import SwiftUI
+
+@main
+struct TokenMeterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
